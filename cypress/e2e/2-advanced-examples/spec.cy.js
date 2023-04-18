4019626694b6def6d655bf5ca4a8d868641215b4
indra <indrahtt@amazon.com>
2023-04-19 02:48:17 +0800

    update

--- cypress/e2e/2-advanced-examples/spec.cy.js
+++ cypress/e2e/2-advanced-examples/spec.cy.js
@@ -2,11 +2,11 @@
 
 describe('Quicksight Visual Regression Test', () => {
   
-    it('Navigate to dashboard', () => {
-      cy.visit(
-        'https://p2ydjsiuqd.execute-api.ap-southeast-1.amazonaws.com/dev/generate-embed'
-      );
-      cy.wait(20000);
-    });
+  it('Navigate to dashboard', () => {
+    cy.visit(
+      'https://p2ydjsiuqd.execute-api.ap-southeast-1.amazonaws.com/dev/generate-embed'
+    );
+    cy.wait(20000);
   });
+});
   
