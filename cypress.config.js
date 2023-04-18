08789443ab719f48faee301338bcefca568c0718
indra <indrahtt@amazon.com>
2023-04-19 01:39:27 +0800

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,15 @@
-module.exports = {
-  'projectId': '4b7344',
+// module.exports = {
+//   'projectId': '4b7344',
+//   e2e: {},
+// }
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
   e2e: {},
-}
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/results-[hash].xml',
+    toConsole: true,
+  },
+})
